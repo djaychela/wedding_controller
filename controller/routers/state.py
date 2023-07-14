61229b3ad985f942c9a92ccfe883850e660e4b68
Darren Jones <darrenjones@MBP2022.fastmail.com>
2023-07-14 15:46:33 +0100

    control bands from mixxx watcher

--- controller/routers/state.py
+++ controller/routers/state.py
@@ -15,13 +15,14 @@ def read_state(db: Session = Depends(get_db)):
     return current_state
 
 @router.post("/set_current_song", response_model=schemas.StateBase)
-def store_current_song(new_state: schemas.StateSetSong, db: Session = Depends(get_db)):
+def store_current_song(new_state: schemas.StateSetSong, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     current_state = state.get_state(db)
     current_state.current_song_id = new_state.current_song_id
     current_state.current_song_title = new_state.current_song_title
     current_state.current_song_artist = new_state.current_song_artist
     db.commit()
     dancefloor.increase_dancefloor_songs(db=db)
+    background_tasks.add_task(api_for_new_song, db, new_state.current_song_id)
 
     return current_state
 
